d1ebfad3670f8fc709d5032ca9bd17dc22c3323c
DaniPV05 <92324288+DaniPV05@users.noreply.github.com>
2021-11-08 22:18:52 -0500

    Add files via upload

---
+++ U.js
@@ -0,0 +1,23 @@
+var n1 , n2 , resultado;
+n1= prompt ("3") ;
+n2= prompt ("2") ;
+resultado = (parseInt (n1)+ parseInt (n2)) ;
+alert ("El resultado es 5") ;
+
+var n3 , n4 , resultado;
+n3= prompt("3") ;
+n4= prompt("2") ;
+resultado = (parseInt(n3)- parseInt(n4)) ;
+alert ("El resultado es -1") ;
+
+var n5 , n6 , resultado;
+n5= prompt("3") ;
+n6= prompt("2") ;
+resultado = (parseInt(n3)* parseInt(n4)) ;
+alert ("El resultado es 4760") ;
+
+var n7 , n8 , resultado;
+n7= prompt("3") ;
+n8= prompt("2") ;
+resultado = (parseInt(n3)/ parseInt(n4)) ;
+alert ("El resultado es 54,16") ;
